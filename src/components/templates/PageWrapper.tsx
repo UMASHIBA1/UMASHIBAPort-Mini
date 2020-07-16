692ec318a01ad4b4900053c0f2a585f9dc058e2c
UMASHIBA <umashiba1@gmail.com>
2020-07-16 22:57:56 +0900

    [MOD]lg用のPageWrapperを変更

--- src/components/templates/PageWrapper.tsx
+++ src/components/templates/PageWrapper.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const PageWrapper: React.FC = (props) => {
-  return <div className="w-full h-full">{props.children}</div>;
+  return (
+    <div className="w-full h-full flex justify-center">
+      <div className="flex  flex-col w-full h-full lg:w-3/5 lg:mt-8">
+        {props.children}
+      </div>
+    </div>
+  );
 };
 
 export default PageWrapper;
