c90749bad493e31069a811f5a15950b56f776016
UMASHIBA <umashiba1@gmail.com>
2020-07-16 22:10:14 +0900

    [SET]tailwindの設定に色に関するものを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "my-dark-brown": "#4D3614",
+        "my-brown": "#6D4845",
+        "my-light-brown": "#AEA5A0",
+        "my-gray": "#EDEAEC",
+        "my-dark-yellow": "#B69B63",
+        "my-light-yellow": "#E8D6AE",
+        "my-red": "#AC291D",
+        "my-green": "#006F5F",
+        "my-blue": "#00426D",
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
