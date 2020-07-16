c9118cc1b19cf543912963fc1ecb6cf8dbb499b2
UMASHIBA <umashiba1@gmail.com>
2020-07-16 22:09:47 +0900

    [MOD]SectionTitleで色を引数に取るように変更

--- src/components/molecules/SectionTitle.tsx
+++ src/components/molecules/SectionTitle.tsx
@@ -3,11 +3,17 @@ import H1 from "../atomics/H1";
 
 interface Props {
   text: string;
+  colorClassName?: string;
 }
 
-const SectionTitle: React.FC<Props> = ({ text }: Props) => {
+const SectionTitle: React.FC<Props> = ({
+  text,
+  colorClassName = "white",
+}: Props) => {
   return (
-    <div className="flex items-center h-full justify-start pl-2">
+    <div
+      className={`flex items-center h-full justify-start pl-2 text-${colorClassName}`}
+    >
       <H1 text={text} />
     </div>
   );
