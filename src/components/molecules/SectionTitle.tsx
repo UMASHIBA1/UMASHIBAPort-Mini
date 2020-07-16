08b039fcc39a9dbdc140d4343bbfae9dbae358e4
UMASHIBA <umashiba1@gmail.com>
2020-07-16 21:10:59 +0900

    [ADD]SectionTitle作成

---
+++ src/components/molecules/SectionTitle.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import H1 from "../atomics/H1";
+
+interface Props {
+  text: string;
+}
+
+const SectionTitle: React.FC<Props> = ({ text }: Props) => {
+  return (
+    <div className="flex items-center h-full justify-center">
+      <H1 text={text} />
+    </div>
+  );
+};
+
+export default SectionTitle;
