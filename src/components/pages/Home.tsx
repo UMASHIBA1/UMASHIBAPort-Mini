77dd303cee625e88b41db634c7556b1617bc3561
UMASHIBA <umashiba1@gmail.com>
2020-07-18 01:41:27 +0900

    [MOD]4つのSectionを作成

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import PageWrapper from "../templates/PageWrapper";
+import Section from "../molecules/Section";
+import aboutMeData from "../../datas/aboutMeData";
+import worksData from "../../datas/worksData";
+import skillsData from "../../datas/skillsData";
+import contactData from "../../datas/contactData";
 
 const Home: React.FC = () => {
   return (
     <PageWrapper>
-      <div />
+      <Section
+        title="About Me"
+        colorClassName="red-500"
+        contentDatas={aboutMeData}
+      />
+      <Section
+        title="Works"
+        colorClassName="blue-500"
+        contentDatas={worksData}
+      />
+      <Section
+        title="Skills"
+        colorClassName="orange-500"
+        contentDatas={skillsData}
+      />
+      <Section
+        title="Contact"
+        colorClassName="green-500"
+        contentDatas={contactData}
+      />
     </PageWrapper>
   );
 };
