14569c3a4239a8420310d1e1423286229b9d95e1
UMASHIBA <umashiba1@gmail.com>
2020-07-18 02:16:54 +0900

    [MOD]SectionContentの内容をモーダル付きのモノに変更

--- src/components/molecules/SectionContent.tsx
+++ src/components/molecules/SectionContent.tsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentData from "../../typings/CotentData";
 import SectionContentCell from "../atomics/SectionContentCell";
+import Modal from "./Modal";
+import H1 from "../atomics/H1";
 
 interface Props {
   contentDatas?: ContentData[];
 }
+
+interface OneContentProps {
+  content: ContentData;
+}
+
+const OneContent: React.FC<OneContentProps> = ({
+  content,
+}: OneContentProps) => {
+  const { title, description, link } = content;
+  const [isDisable, changeIsDisable] = useState(true);
+  return (
+    <React.Fragment>
+      <Modal isDisable={isDisable} onClickBG={() => changeIsDisable(true)}>
+        <div className="p-2 m-2">
+          <H1 text={title} />
+          <div className="p-2">{description}</div>
+        </div>
+      </Modal>
+      <SectionContentCell onClickFC={() => changeIsDisable(false)}>
+        <div className="break-words">{title}</div>
+      </SectionContentCell>
+    </React.Fragment>
+  );
+};
 
 const SectionContent: React.FC<Props> = ({ contentDatas }: Props) => {
   return (
     <div className="w-full h-full flex justify-start flex-row flex-wrap pt-2">
       {contentDatas !== undefined
         ? contentDatas.map((content) => {
-            return (
-              <SectionContentCell>
-                <div className="break-words">{content.title}</div>
-              </SectionContentCell>
-            );
+            return <OneContent content={content} />;
           })
         : null}
     </div>
