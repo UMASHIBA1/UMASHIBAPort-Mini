ffb65941420724a082c538272ce14195148a6501
UMASHIBA <umashiba1@gmail.com>
2020-07-18 01:41:06 +0900

    [ADD]Section作成

---
+++ src/components/molecules/Section.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import SectionHeader from "./SectionHeader";
+import SectionContent from "./SectionContent";
+import ContentData from "../../typings/CotentData";
+
+interface Props {
+  title: string;
+  colorClassName: string;
+  contentDatas?: ContentData[];
+}
+
+const Section: React.FC<Props> = ({
+  title,
+  colorClassName,
+  contentDatas,
+}: Props) => {
+  return (
+    <section className="w-full h-full flex flex-col justify-center p-2">
+      <SectionHeader title={title} colorClassName={colorClassName} />
+      <SectionContent contentDatas={contentDatas} />
+    </section>
+  );
+};
+
+export default Section;
