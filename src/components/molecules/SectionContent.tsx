9df594c1b124968671a43e8f33c8e3da18a39bfc
UMASHIBA <umashiba1@gmail.com>
2020-07-18 01:40:57 +0900

    [ADD]SectionContent作成

---
+++ src/components/molecules/SectionContent.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ContentData from "../../typings/CotentData";
+import SectionContentCell from "../atomics/SectionContentCell";
+
+interface Props {
+  contentDatas?: ContentData[];
+}
+
+const SectionContent: React.FC<Props> = ({ contentDatas }: Props) => {
+  return (
+    <div className="w-full h-full flex justify-start flex-row flex-wrap pt-2">
+      {contentDatas !== undefined
+        ? contentDatas.map((content) => {
+            return <SectionContentCell>{content.title}</SectionContentCell>;
+          })
+        : null}
+    </div>
+  );
+};
+
+export default SectionContent;
