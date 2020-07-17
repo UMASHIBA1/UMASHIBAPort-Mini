790d0a8c943c2eb0e1eff0774e782db99e305345
UMASHIBA <umashiba1@gmail.com>
2020-07-18 02:13:57 +0900

    [ADD]Modalを作成

---
+++ src/components/molecules/Modal.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+import Overlay from "../atomics/Overlay";
+
+interface Props {
+  isDisable: boolean;
+  children: ReactNode;
+  onClickBG: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+
+const Modal: React.FC<Props> = (props: Props) => {
+  return (
+    <Overlay isDisable={props.isDisable} onClick={props.onClickBG}>
+      <div className="bg-gray-200 rounded-md">{props.children}</div>
+    </Overlay>
+  );
+};
+
+export default Modal;
